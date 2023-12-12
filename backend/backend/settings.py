72b20e4186667827d667834445562cc8158b6e6e
Damodar <151498795+ismarthawkback@users.noreply.github.com>
2023-12-12 09:14:28 +0000

    feat: Update database settings
    Switched database engine from sqlite3 to MySQL and updated connection details. and updated the database to mariadb

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -90,8 +90,15 @@ WSGI_APPLICATION = 'backend.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'asterisk',
+        'USER': 'root',
+        'PASSWORD': 'root',
+        'HOST': 'localhost',
+        'PORT': '3306',
+        'OPTIONS': {
+            'init_command': "SET sql_mode='STRICT_TRANS_TABLES'"
+        }
     }
 }
 
@@ -121,7 +128,6 @@ REST_FRAMEWORK = {
         'rest_framework_simplejwt.authentication.JWTStatelessUserAuthentication',
     )
 }
-
 
 
 SIMPLE_JWT = {
