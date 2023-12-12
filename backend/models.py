14ccbc3f6cee85ed47990a56f0ea83f2d82250e2
Damodar <151498795+ismarthawkback@users.noreply.github.com>
2023-12-12 10:53:41 +0000

    Update the models.py

--- backend/models.py
+++ backend/models.py
@@ -1,10 +1,59 @@
+# This is an auto-generated Django model module.
+# You'll have to do the following manually to clean this up:
+#   * Rearrange models' order
+#   * Make sure each model has one field with primary_key=True
+#   * Make sure each ForeignKey and OneToOneField has `on_delete` set to the desired behavior
+#   * Remove `managed = False` lines if you wish to allow Django to create, modify, and delete the table
+# Feel free to rename the models, but don't rename db_table values or field names.
 from django.db import models
 
-# Create your models here.
+
+class AccountsUseraccount(models.Model):
+    id = models.BigAutoField(primary_key=True)
+    password = models.CharField(max_length=128)
+    last_login = models.DateTimeField(blank=True, null=True)
+    email = models.CharField(unique=True, max_length=254)
+    username = models.CharField(max_length=255)
+    is_active = models.IntegerField()
+    is_admin = models.IntegerField()
+    is_staff = models.IntegerField()
+    is_superuser = models.IntegerField()
+    level = models.CharField(max_length=20)
+
+    class Meta:
+        managed = False
+        db_table = 'accounts_useraccount'
 
 
-class Book(models.Model):
+class AccountsUseraccountGroups(models.Model):
+    id = models.BigAutoField(primary_key=True)
+    useraccount = models.ForeignKey(AccountsUseraccount, models.DO_NOTHING)
+    group = models.ForeignKey('AuthGroup', models.DO_NOTHING)
+
+    class Meta:
+        managed = False
+        db_table = 'accounts_useraccount_groups'
+        unique_together = (('useraccount', 'group'),)
+
+
+class AccountsUseraccountUserPermissions(models.Model):
+    id = models.BigAutoField(primary_key=True)
+    useraccount = models.ForeignKey(AccountsUseraccount, models.DO_NOTHING)
+    permission = models.ForeignKey('AuthPermission', models.DO_NOTHING)
+
+    class Meta:
+        managed = False
+        db_table = 'accounts_useraccount_user_permissions'
+        unique_together = (('useraccount', 'permission'),)
+
+
+class ApiBook(models.Model):
+    id = models.BigAutoField(primary_key=True)
     name = models.CharField(max_length=255)
+
+    class Meta:
+        managed = False
+        db_table = 'api_book'
 
 
 class AstConfbridge(models.Model):
@@ -119,18 +168,10 @@ class AstSipfriends(models.Model):
     maxcallbitrate = models.IntegerField(blank=True, null=True)
     rfc2833compensate = models.CharField(max_length=3, blank=True, null=True)
     mailbox = models.CharField(max_length=40, blank=True, null=True)
-    # Field renamed to remove unsuitable characters.
-    session_timers = models.CharField(
-        db_column='session-timers', max_length=9, blank=True, null=True)
-    # Field renamed to remove unsuitable characters.
-    session_expires = models.IntegerField(
-        db_column='session-expires', blank=True, null=True)
-    # Field renamed to remove unsuitable characters.
-    session_minse = models.IntegerField(
-        db_column='session-minse', blank=True, null=True)
-    # Field renamed to remove unsuitable characters.
-    session_refresher = models.CharField(
-        db_column='session-refresher', max_length=3, blank=True, null=True)
+    session_timers = models.CharField(db_column='session-timers', max_length=9, blank=True, null=True)  # Field renamed to remove unsuitable characters.
+    session_expires = models.IntegerField(db_column='session-expires', blank=True, null=True)  # Field renamed to remove unsuitable characters.
+    session_minse = models.IntegerField(db_column='session-minse', blank=True, null=True)  # Field renamed to remove unsuitable characters.
+    session_refresher = models.CharField(db_column='session-refresher', max_length=3, blank=True, null=True)  # Field renamed to remove unsuitable characters.
     t38pt_usertpsource = models.CharField(max_length=40, blank=True, null=True)
     regexten = models.CharField(max_length=40, blank=True, null=True)
     fromdomain = models.CharField(max_length=40, blank=True, null=True)
@@ -165,9 +206,7 @@ class AstSipfriends(models.Model):
     vmexten = models.CharField(max_length=40, blank=True, null=True)
     autoframing = models.CharField(max_length=3, blank=True, null=True)
     rtpkeepalive = models.IntegerField(blank=True, null=True)
-    # Field renamed to remove unsuitable characters.
-    call_limit = models.IntegerField(
-        db_column='call-limit', blank=True, null=True)
+    call_limit = models.IntegerField(db_column='call-limit', blank=True, null=True)  # Field renamed to remove unsuitable characters.
     g726nonstandard = models.CharField(max_length=3, blank=True, null=True)
     ignoresdpversion = models.CharField(max_length=3, blank=True, null=True)
     allowtransfer = models.CharField(max_length=3, blank=True, null=True)
@@ -209,6 +248,104 @@ class AstVoicemailUsers(models.Model):
     class Meta:
         managed = False
         db_table = 'ast_voicemail_users'
+
+
+class AuthGroup(models.Model):
+    name = models.CharField(unique=True, max_length=150)
+
+    class Meta:
+        managed = False
+        db_table = 'auth_group'
+
+
+class AuthGroupPermissions(models.Model):
+    id = models.BigAutoField(primary_key=True)
+    group = models.ForeignKey(AuthGroup, models.DO_NOTHING)
+    permission = models.ForeignKey('AuthPermission', models.DO_NOTHING)
+
+    class Meta:
+        managed = False
+        db_table = 'auth_group_permissions'
+        unique_together = (('group', 'permission'),)
+
+
+class AuthPermission(models.Model):
+    name = models.CharField(max_length=255)
+    content_type = models.ForeignKey('DjangoContentType', models.DO_NOTHING)
+    codename = models.CharField(max_length=100)
+
+    class Meta:
+        managed = False
+        db_table = 'auth_permission'
+        unique_together = (('content_type', 'codename'),)
+
+
+class DjangoAdminLog(models.Model):
+    action_time = models.DateTimeField()
+    object_id = models.TextField(blank=True, null=True)
+    object_repr = models.CharField(max_length=200)
+    action_flag = models.PositiveSmallIntegerField()
+    change_message = models.TextField()
+    content_type = models.ForeignKey('DjangoContentType', models.DO_NOTHING, blank=True, null=True)
+    user = models.ForeignKey(AccountsUseraccount, models.DO_NOTHING)
+
+    class Meta:
+        managed = False
+        db_table = 'django_admin_log'
+
+
+class DjangoContentType(models.Model):
+    app_label = models.CharField(max_length=100)
+    model = models.CharField(max_length=100)
+
+    class Meta:
+        managed = False
+        db_table = 'django_content_type'
+        unique_together = (('app_label', 'model'),)
+
+
+class DjangoMigrations(models.Model):
+    id = models.BigAutoField(primary_key=True)
+    app = models.CharField(max_length=255)
+    name = models.CharField(max_length=255)
+    applied = models.DateTimeField()
+
+    class Meta:
+        managed = False
+        db_table = 'django_migrations'
+
+
+class DjangoSession(models.Model):
+    session_key = models.CharField(primary_key=True, max_length=40)
+    session_data = models.TextField()
+    expire_date = models.DateTimeField()
+
+    class Meta:
+        managed = False
+        db_table = 'django_session'
+
+
+class TokenBlacklistBlacklistedtoken(models.Model):
+    id = models.BigAutoField(primary_key=True)
+    blacklisted_at = models.DateTimeField()
+    token = models.OneToOneField('TokenBlacklistOutstandingtoken', models.DO_NOTHING)
+
+    class Meta:
+        managed = False
+        db_table = 'token_blacklist_blacklistedtoken'
+
+
+class TokenBlacklistOutstandingtoken(models.Model):
+    id = models.BigAutoField(primary_key=True)
+    token = models.TextField()
+    created_at = models.DateTimeField(blank=True, null=True)
+    expires_at = models.DateTimeField()
+    user = models.ForeignKey(AccountsUseraccount, models.DO_NOTHING, blank=True, null=True)
+    jti = models.CharField(unique=True, max_length=255)
+
+    class Meta:
+        managed = False
+        db_table = 'token_blacklist_outstandingtoken'
 
 
 class VoicemailUsers(models.Model):
