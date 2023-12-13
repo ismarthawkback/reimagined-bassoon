19a02423209a7f645dfd881859e1bc8569527056
Damodar <151498795+ismarthawkback@users.noreply.github.com>
2023-12-13 04:44:23 +0000

    Routes and authcontext done

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,20 +1,22 @@
-import Typography from '@mui/material/Typography'
-import { ThemeContext } from './contexts/ThemeContext'
-import { useContext } from 'react'
-import { createTheme, CssBaseline, ThemeProvider } from '@mui/material'
-import ThemeToggle from './components/navbar-components/ThemeToggle'
-import Navbar from './components/Navbar'
-import LoginPage from './pages/LoginPage'
-import HomePage from './pages/HomePage'
-import {Routes, Route} from 'react-router-dom'
-import DatabasePage from './pages/DatabasePage'
-import DatabaseTables from './components/DatabasePage-components/Home/DatabaseTables'
-import DatabaseTable from './components/DatabasePage-components/Table/DatabaseTable'
-import DatabaseForm from './components/DatabasePage-components/Form/DatabaseForm'
+import Typography from "@mui/material/Typography";
+import { ThemeContext } from "./contexts/ThemeContext";
+import { useContext } from "react";
+import { createTheme, CssBaseline, ThemeProvider } from "@mui/material";
+import ThemeToggle from "./components/navbar-components/ThemeToggle";
+import Navbar from "./components/Navbar";
+import LoginPage from "./pages/LoginPage";
+import HomePage from "./pages/HomePage";
+import { Routes, Route } from "react-router-dom";
+import DatabasePage from "./pages/DatabasePage";
+import DatabaseTables from "./components/DatabasePage-components/Home/DatabaseTables";
+import DatabaseTable from "./components/DatabasePage-components/Table/DatabaseTable";
+import DatabaseForm from "./components/DatabasePage-components/Form/DatabaseForm";
 
+import ProtectedRoute from "./utils/ProtectedRoute";
+import { AuthContextProvider } from "./contexts/AuthContext";
 
 function App() {
-  const {theme, toggleTheme} = useContext(ThemeContext)
+  const { theme, toggleTheme } = useContext(ThemeContext);
 
   const appTheme = createTheme({
     palette: {
@@ -24,29 +26,46 @@ function App() {
 
   return (
     <>
-    <ThemeProvider theme={appTheme}>
-      <CssBaseline />
-      <Navbar />
-      <Routes>
-        <Route path='/' element={<HomePage />} />
-        <Route path='login' element={<LoginPage />} />
-        <Route path='database' element={<DatabasePage />}>
+      <ThemeProvider theme={appTheme}>
+        <CssBaseline />
+        <AuthContextProvider>
+          <Navbar />
+          <Routes>
+            <Route
+              path=""
+              element={
+                <ProtectedRoute>
+                  <HomePage />
+                </ProtectedRoute>
+              }
+            >
+              <Route path="database" element={<DatabasePage />}>
+                <Route path="" element={<DatabaseTables />} />
+                <Route path=":table" element={<DatabaseTable />} />
+                <Route path=":table/new" element={<DatabaseForm />} />
+                <Route path=":table/:id" element={<DatabaseForm />} />
+              </Route>
+            </Route>
+            <Route path="login" element={<LoginPage />} />
+
+            {/* <Route path="" element={<HomePage />} />
+          <Route path="login" element={<LoginPage />} />
+          <Route path="database" element={<DatabasePage />}>
             <Route path="" element={<DatabaseTables />} />
-            <Route path=':table' element={<DatabaseTable />} /> 
-            <Route path=':table/new' element={<DatabaseForm />} />
-            <Route path=':table/:id' element={<DatabaseForm />} />
-        </Route>
-      </Routes>
-      {/* <DatabasePage /> */}
-      {/* <LoginPage />
+            <Route path=":table" element={<DatabaseTable />} />
+            <Route path=":table/new" element={<DatabaseForm />} />
+            <Route path=":table/:id" element={<DatabaseForm />} />
+          </Route> */}
+          </Routes>
+          {/* <DatabasePage /> */}
+          {/* <LoginPage />
       <HomePage /> */}
 
-      {/* <ThemeToggle /> */}
-      
-    </ThemeProvider>
-      
+          {/* <ThemeToggle /> */}
+        </AuthContextProvider>
+      </ThemeProvider>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
