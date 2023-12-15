2fd0f9f296ff3ce1b99072f4f93f918a5fdc9d6b
Damodar <151498795+ismarthawkback@users.noreply.github.com>
2023-12-15 06:17:36 +0000

    Login and logout done

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -10,15 +10,17 @@ import MenuItem from "@mui/material/MenuItem";
 import Menu from "@mui/material/Menu";
 import { Stack } from "@mui/material";
 import SideDrawer from "./navbar-components/SideDrawer";
-import { useState } from "react";
+import { useState, useContext } from "react";
+import authContext from "../contexts/AuthContext";
 
 export default function Navbar() {
   const [anchorEl, setAnchorEl] = useState(null);
   const [drawerOpen, setDrawerOpen] = useState(false);
+  const { user, logout } = useContext(authContext);
+
   const setDrawerState = (open) => {
-    setDrawerOpen(open)
-  }
- 
+    setDrawerOpen(open);
+  };
 
   const handleMenu = (event) => {
     setAnchorEl(event.currentTarget);
@@ -30,60 +32,84 @@ export default function Navbar() {
 
   return (
     <Box sx={{ flexGrow: 1 }}>
-      <AppBar position="sticky" sx={{marginBottom : 2}}>
+      <AppBar position="sticky" sx={{ marginBottom: 2 }}>
         <Toolbar>
-          <IconButton
-            size="large"
-            edge="start"
-            color="inherit"
-            aria-label="menu"
-            onClick={() => setDrawerOpen(true)}
-          >
-            <MenuIcon />
-          </IconButton>
-          <img src='/cdot_logo_half.png' style={{
-            height : '40px', width : '35px', marginRight : '10px', objectFit : 'contain'
-          }}/>
+          {user !== null && (
+            <IconButton
+              size="large"
+              edge="start"
+              color="inherit"
+              aria-label="menu"
+              onClick={() => setDrawerOpen(true)}
+            >
+              <MenuIcon />
+            </IconButton>
+          )}
+          <img
+            src="/cdot_logo_half.png"
+            style={{
+              height: "40px",
+              width: "35px",
+              marginRight: "10px",
+              objectFit: "contain",
+            }}
+          />
           <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
             CDOT VoIP
           </Typography>
 
-          <Stack direction='row'  sx={{alignItems : 'center'}}>
+          <Stack direction="row" sx={{ alignItems: "center" }}>
             {/* <Stack>
                 <ThemeToggle />
                 <Typography variant="boday2">Dark Mode</Typography>
             </Stack> */}
-            <Typography variant="body1">Hello, Damodar</Typography>
-            <IconButton
-              size="large"
-              aria-controls="menu-appbar"
-              aria-haspopup="true"
-              onClick={handleMenu}
-              color="inherit"
-            >
-              <AccountCircle fontSize="large"/>
-            </IconButton>
-            <Menu
-              id="menu-appbar"
-              anchorEl={anchorEl}
-              anchorOrigin={{
-                vertical: "top",
-                horizontal: "right",
-              }}
-              keepMounted
-              transformOrigin={{
-                vertical: "top",
-                horizontal: "right",
-              }}
-              open={Boolean(anchorEl)}
-              onClose={handleClose}
-            >
-              <MenuItem onClick={handleClose}>Logout</MenuItem>
-            </Menu>
+            {user === null ? (
+              <>
+                <Typography variant="body1">Please Login</Typography>
+              </>
+            ) : (
+              <>
+                <Typography variant="body1">Hello, Damodar</Typography>
+                <IconButton
+                  size="large"
+                  aria-controls="menu-appbar"
+                  aria-haspopup="true"
+                  onClick={handleMenu}
+                  color="inherit"
+                >
+                  <AccountCircle fontSize="large" />
+                </IconButton>
+
+                <Menu
+                  id="menu-appbar"
+                  anchorEl={anchorEl}
+                  anchorOrigin={{
+                    vertical: "top",
+                    horizontal: "right",
+                  }}
+                  keepMounted
+                  transformOrigin={{
+                    vertical: "top",
+                    horizontal: "right",
+                  }}
+                  open={Boolean(anchorEl)}
+                  onClose={handleClose}
+                >
+                  <MenuItem
+                    onClick={async () => {
+                      await logout();
+                      handleClose();
+                    }}
+                  >
+                    Logout
+                  </MenuItem>
+                </Menu>
+              </>
+            )}
           </Stack>
         </Toolbar>
       </AppBar>
-      <SideDrawer drawerOpen={drawerOpen} setDrawerState={setDrawerState}/>
+      <SideDrawer drawerOpen={drawerOpen} setDrawerState={setDrawerState} />
     </Box>
   );
 }
